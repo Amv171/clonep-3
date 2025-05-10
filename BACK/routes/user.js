0de0a18bf0fdaef98e876267682e5642153e7f02
Antonio Moreno <moreno.villas.17@gmail.com>
2025-05-10 12:18:52 +0200

    GeGet users

--- BACK/routes/user.js
+++ BACK/routes/user.js
@@ -20,7 +20,41 @@ const {authMiddleware, checkRol} = require('../middleware/session');
 
 
 
-
+//Ruta para obtener todos los usuarios
+/**
+ * @swagger
+ * /api/user:
+ *   get:
+ *     summary: Get all users (admin only)
+ *     tags: [Users]
+ *     description: Retrieves a list of all users from the database. Requires admin role.
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: A list of users.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: string
+ *                     description: The ID of the user.
+ *                   name:
+ *                     type: string
+ *                     description: The name of the user.
+ *                   email:
+ *                     type: string
+ *                     description: The email of the user.
+ *       401:
+ *         description: Unauthorized. Token is missing or invalid.
+ *       403:
+ *         description: Forbidden. User does not have the required role.
+ */ 
+router.get("/", authMiddleware, checkRol(["admin"]), getItems);
 
 
 
